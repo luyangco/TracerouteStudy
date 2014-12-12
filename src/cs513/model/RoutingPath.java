58b0a1c07b43f57c4d28ad15b81895f1697fda26
Yang Lu <luyang.co@gmail.com>
2014-12-12 13:00:51 -0500

    Incomplete work on erroneous routing

--- src/cs513/model/RoutingPath.java
+++ src/cs513/model/RoutingPath.java
@@ -1,16 +1,21 @@
 package cs513.model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class RoutingPath {
 	ArrayList <IPaddress> m_path;
+	IPaddress m_srcIP;
 	IPaddress m_destIP;
-	Calendar m_timestamp;
+	String m_timestamp;
 	
 
-	public RoutingPath(IPaddress dest) {
+	public RoutingPath(IPaddress src, IPaddress dest) {
 		 m_path = new ArrayList<IPaddress>();
+		 m_srcIP = src;
 		 m_destIP = dest;
 	}
 	
@@ -26,11 +31,65 @@ public class RoutingPath {
 		return m_destIP;
 	}
 
-	public Calendar getTimestamp() {
+	public String getTimestamp() {
 		return m_timestamp;
 	}
 
-	public void setTimestamp(Calendar m_timestamp) {
+	public void setTimestamp(String m_timestamp) {
 		this.m_timestamp = m_timestamp;
+	}
+
+	public IPaddress getSrcIP() {
+		return m_srcIP;
+	}
+	
+	public IPaddress getLastHop() {
+		return m_path.get(m_path.size() - 1);
+	}
+
+	public boolean within2Hrs(String timestamp) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd.hh-mm-ss");
+		sdf.setLenient(false);
+
+		// if not valid, it will throw ParseException
+		Date date;
+		try {
+			date = sdf.parse(timestamp);
+
+
+			// current date after 1 hour
+			Calendar currentDateAfter1Hr = Calendar.getInstance();
+			currentDateAfter1Hr.add(Calendar.HOUR, 1);
+
+			// current date before 1 hour
+			Calendar currentDateBefore1Hr = Calendar.getInstance();
+			currentDateBefore1Hr.add(Calendar.HOUR, -1);
+
+			/*************** verbose ***********************/
+			System.out.println("\n\ncurrentDate : "
+					+ Calendar.getInstance().getTime());
+			System.out.println("currentDateAfter3Months : "
+					+ currentDateAfter1Hr.getTime());
+			System.out.println("currentDateBefore3Months : "
+					+ currentDateBefore1Hr.getTime());
+			System.out.println("dateToValidate : " + timestamp);
+			/************************************************/
+
+			if (date.before(currentDateAfter1Hr.getTime())
+					&& date.after(currentDateBefore1Hr.getTime())) {
+
+				//ok everything is fine, date in range
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
 	}
 }
--- src/cs513/parser/OutputParser.java
+++ src/cs513/parser/OutputParser.java
@@ -18,6 +18,46 @@ import cs513.model.RoutingPath;
 public class OutputParser {
 	public HashMap<String, ArrayList<RoutingPath>> m_hostmap = new HashMap<String, ArrayList<RoutingPath>>();
 	
+	private static HashMap<String, IPaddress> m_mapping = new HashMap<String, IPaddress>();
+	static {
+		m_mapping.put("planetlab2.csee.usf.edu", new IPaddress("131.247.2.242"));
+		m_mapping.put("planetlab1.csuohio.edu", new IPaddress("137.148.16.10"));
+		m_mapping.put("planetlab1.acis.ufl.edu", new IPaddress("128.227.150.11"));
+		m_mapping.put("ricepl-1.cs.rice.edu", new IPaddress("128.42.142.41"));
+		m_mapping.put("planetlab1.cs.pitt.edu", new IPaddress("192.52.240.213"));
+		m_mapping.put("pl3.cs.unm.edu", new IPaddress("198.83.85.46"));
+		m_mapping.put("planetlab2.cis.upenn.edu", new IPaddress("158.130.6.253"));
+		m_mapping.put("planetlab1.clemson.edu", new IPaddress("130.127.39.152"));
+		m_mapping.put("righthand.eecs.harvard.edu", new IPaddress("140.247.60.126"));
+		m_mapping.put("planetlab1.utdallas.edu", new IPaddress("129.110.125.51"));
+		m_mapping.put("planetlab-3.cmcl.cs.cmu.edu", new IPaddress("128.2.211.115"));
+		m_mapping.put("75-130-96-13.static.oxfr.ma.charter.com", new IPaddress("75.130.96.13"));
+		m_mapping.put("planetlab1.cs.du.edu", new IPaddress("130.253.21.121"));
+		m_mapping.put("pl-node-1.csl.sri.com", new IPaddress("192.12.33.102"));
+		m_mapping.put("planetlab-4.eecs.cwru.edu", new IPaddress("129.22.150.29"));
+		m_mapping.put("planetlab4.ie.cuhk.edu.hk", new IPaddress("137.189.98.209"));
+		m_mapping.put("planet-lab4.uba.ar", new IPaddress("157.92.44.104"));
+		m_mapping.put("plonk.cs.uwaterloo.ca", new IPaddress("129.97.74.14"));
+		m_mapping.put("planetlab2.koganei.itrc.net", new IPaddress("133.69.32.133"));
+		m_mapping.put("planetlab3.cesnet.cz", new IPaddress("195.113.161.84"));
+		m_mapping.put("planetlab1.dojima.wide.ad.jp", new IPaddress("203.178.133.11"));
+		m_mapping.put("planetlab1.netlab.uky.edu", new IPaddress("128.163.142.20"));
+		m_mapping.put("planetlab1.postel.org", new IPaddress("206.117.37.4"));
+		m_mapping.put("planetlab1.cti.espol.edu.ec", new IPaddress("200.10.150.252"));
+		m_mapping.put("planetlab1.cs.otago.ac.nz", new IPaddress("139.80.206.132"));
+		m_mapping.put("planet-plc-1.mpi-sws.org", new IPaddress("139.19.158.225"));
+		m_mapping.put("pl1.eng.monash.edu.au", new IPaddress("130.194.252.8"));
+		m_mapping.put("planetlab2.cs.okstate.edu", new IPaddress("139.78.141.245"));
+		m_mapping.put("planetlab1.ustc.edu.cn", new IPaddress("202.141.161.43"));
+		m_mapping.put("planetlab-2.sjtu.edu.cn", new IPaddress("202.112.28.100"));
+		m_mapping.put("planetlab-01.vt.nodes.planet-lab.org", new IPaddress("198.82.160.238"));
+		m_mapping.put("pl1.6test.edu.cn", new IPaddress("219.243.208.60"));
+		m_mapping.put("pli1-pa-6.hpl.hp.com", new IPaddress("204.123.28.57"));
+		m_mapping.put("planetlab3.inf.ethz.ch", new IPaddress("192.33.90.68"));
+		m_mapping.put("planetlab2.cs.unc.edu", new IPaddress("204.85.191.11"));
+		m_mapping.put("planetlab-1.scie.uestc.edu.cn", new IPaddress("222.197.180.137"));
+	}
+	
 	public Calendar convertToCalendar(String date) {
 		
 		String TIMESTAMP_PATTERN = "(?:(\\d{4})-(\\d{2})-(\\d{2})\\.(\\d{2})-(\\d{2})-(\\d{2})?)";
@@ -94,10 +134,10 @@ public class OutputParser {
 			RoutingPath path = null;
 			while ((line = bufferedReader.readLine()) != null) {
 				if (line.startsWith("Dest: ")) {
-					  path = new RoutingPath(string2IP(line)); 
+					  path = new RoutingPath(m_mapping.get(file.getName().substring(0, file.getName().length() - 4)), string2IP(line)); 
 				} else if (line.startsWith("Timestamp: ")) {
 					String ts[] = line.split(" ");
-					path.setTimestamp(convertToCalendar(ts[1]));
+					path.setTimestamp((ts[1]));
 				} else if (line.startsWith("####")) { // detect a new line
 					pathList.add(path);
 				} else {
